982b67fc24450fd0dc46c5a6bca0fd63c0181d69
Dmitriy Kozmenko <dmitriyk@thecpigroup.com>
2013-05-22 10:01:16 -0400

    Added: remove far points

--- pclBox.cpp
+++ pclBox.cpp
@@ -16,10 +16,34 @@
 #include <pcl/filters/statistical_outlier_removal.h>
 
 
+// constants
+
+#define MAXDISTANCE       1.2f      // set max work distance
+
+
 // openNI/PCL get box dimension class
 
 class _3dBoxScan {
 private:
+
+    /**
+     * Copy cloud except far points
+     * @param cloud
+     * @param closer_cloud
+     * @param max_distance
+     */
+    void getCloserPoints(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud,
+            pcl::PointCloud<pcl::PointXYZ>::Ptr &closer_cloud, float max_distance) {
+
+        for (size_t i = 0; i < cloud->size(); i++) {
+            if (cloud->points[i].z < max_distance) {
+                closer_cloud->points.push_back(cloud->points[i]);
+            }
+        }
+
+        return;
+    }
+
 public:
 
     pcl::visualization::CloudViewer viewer;
@@ -29,8 +53,12 @@ public:
 
     void cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &raw_cloud) {
 
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        
+        getCloserPoints(raw_cloud, cloud, MAXDISTANCE);
+        
         if (!viewer.wasStopped()) {
-            viewer.showCloud(raw_cloud);
+            viewer.showCloud(cloud);
         }
 
         return;
