ea81c12f42545d66f4c76e12057c12becfe0ab41
Dmitriy Kozmenko <dmitriyk@thecpigroup.com>
2013-05-22 09:40:50 -0400

    Added: simple 3d class files

---
+++ CMakeFiles/pclBox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pclBox.dir/pclBox.cpp.o"
+  "pclBox.pdb"
+  "pclBox"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pclBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
