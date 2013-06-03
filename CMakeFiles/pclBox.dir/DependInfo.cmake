6d43e75eccf14afd3718d29514436638e6168be2
Dmitriy Kozmenko <dmitriyk@thecpigroup.com>
2013-06-03 10:08:47 -0400

    Added: Show box size

--- CMakeFiles/pclBox.dir/DependInfo.cmake
+++ CMakeFiles/pclBox.dir/DependInfo.cmake
@@ -27,10 +27,12 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/include/vtk-5.8"
-  "/usr/include/pcl-1.6"
+  "/usr/local/include/pcl-1.7"
   "/usr/include/eigen3"
+  "/usr/include/pcl-1.6"
   "/usr/include/openni"
   "/usr/include/qhull"
+  "/usr/include/ni"
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
